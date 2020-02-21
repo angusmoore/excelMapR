902d202fe16b88cdb0b335586857f112d4e05ab7
Angus Moore <18376371+angusmoore@users.noreply.github.com>
2020-02-21 11:15:41 +1100

    Tweak JVM parameters

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
   # Load Java dependencies (all jars inside the java subfolder)
   if (is.null(unlist(options("java.parameters")))) {
-    options(java.parameters = "-XX:-UseGCOverheadLimit -Xmx16384m")
+    options(java.parameters = "-UseGCOverheadLimit -Xmx16384m")
   }
   rJava::.jpackage(name = pkgname, jars = "*")
   cat(paste0("JVM started with parameters: ", options()$java.parameters))
